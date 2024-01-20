c8cb144bbe5ef1eb58781a6ca435035d8142a782
Lupitot <paul.piauger2@gmail.com>
2024-01-20 13:25:50 +0100

    🐛| fix bug login signup : token undefind

--- mamithon/src/services/authService.js
+++ mamithon/src/services/authService.js
@@ -2,14 +2,14 @@
 import axios from 'axios';
 
 export function login(email, password) {
-  return axios.get('http://localhost:3030/api/users/', {
+  return axios.post('http://localhost:3030/api/users/login', {
     "email" : email,
     "password" : password,
   })
   .then(response => {
     console.log('api response', response); 
     localStorage.setItem('token', response.data.token);
-    localStorage.setItem('username', response.data.name);
+    localStorage.setItem('username', response.data.user.username);
 
     return{
       token: response.data.token,
